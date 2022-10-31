b7147b357cad38d9145796dd58039e9dea94155d
Amit El Al <109466957+exize1@users.noreply.github.com>
2022-10-31 19:11:46 +0200

    Update UserSchema.js

--- middlewares/validation/UserSchema.js
+++ middlewares/validation/UserSchema.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 const UserSchema = Joi.object().keys({
-    firstName : Joi.string().alphanum().required(),
-    lastName : Joi.string().alphanum().required(),
+    firstName : Joi.string().required(),
+    lastName : Joi.string().required(),
     email : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net']}}),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
 })
